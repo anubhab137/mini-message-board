9be6e40283ebc9e87f527ff0adaca0d5c1ac861c
anubhab137 <adamsanubhab@gmail.com>
2020-12-20 02:00:41 +0530

    Unfinished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
+const messages = [
+  {
+    text: "Why do you only call me when you're high",
+    user: "Arabella",
+    added: new Date()
+  },
+  {
+    text: "Because you sound kinda boring when I'm sober",
+    user: "Alex",
+    added: new Date()
+  }
+];
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+
+router.post('/new',(req,res,next)=>{
+  
+  messages.push({text: req.body.Message, user: req.body.Name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
