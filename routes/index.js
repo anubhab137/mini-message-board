4e221d9e8bc26b84bb4ed0a0de2c82a57c6ce29a
anubhab137 <adamsanubhab@gmail.com>
2020-12-20 14:16:53 +0530

    Raw

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,22 @@ const messages = [
 
 var express = require('express');
 var router = express.Router();
+var url= require('url');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages });
 });
 
+
+
 router.post('/new',(req,res,next)=>{
   
-  messages.push({text: req.body.Message, user: req.body.Name, added: new Date()});
+  messages.push({text: req.body.Message, user: req.body.Name, 'added': new Date()});
+  console.log(req.baseUrl);
+  console.log(req.path);
   res.redirect('/');
-})
+});
 
 module.exports = router;
